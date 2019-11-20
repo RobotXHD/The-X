aa74ecdbb1d517e3625f179f5b3d995cab69ea10
carol8 <cristib_2002@yahoo.com>
2019-11-20 21:55:15 +0200

    Hopefully fixed rotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomEsuat.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomEsuat.java
@@ -22,29 +22,31 @@ import static java.lang.Math.PI;
 public class AutonomEsuat extends LinearOpMode {
     private OpenCvCamera webcam;
     private int resWidth = 800, resHeight = 448;
-    private Point p1 = new Point(resHeight * 0.55,0);
-    private Point p2 = new Point(resHeight,resWidth);
+    private Point p1 = new Point(resHeight * 0.55, 0);
+    private Point p2 = new Point(resHeight, resWidth);
     private StoneDetectorModified stoneDetectorModified = new StoneDetectorModified(p1, p2);
     private RevBulkData bulkData;
     private ExpansionHubMotor encoderDreapta, encoderSpate, encoderStanga;
     private ExpansionHubEx expansionHub;
     private double forward, right, clockwise, tempforward, tempright;
-    double lastCorrectedY = 0, lastCorrectedX = 0, correctedY = 0, correctedX = 0, deltaY, deltaX,sindeltay,sindeltax, cosdeltay,cosdeltax;
+    double lastCorrectedY = 0, lastCorrectedX = 0, correctedY = 0, correctedX = 0, deltaY, deltaX, sindeltay, sindeltax, cosdeltay, cosdeltax;
     double currentY, currentX, encRot;
-    volatile double angle; // sens trigonometric +
+    volatile double robotHeading, lastRobotHeading; // sens trigonometric +
     double d = 377, omniLengthMm = 188.49555921538759430775860299677, mmPerTick = omniLengthMm / 4000, rotationCircleLenght = PI * d, tickPerDeg = (rotationCircleLenght / omniLengthMm / 360) * 4000;
     long EncSp, EncSt, EncDr, lastEncDr;
     boolean stop = false;
     volatile boolean encodereCitite = false;
-    long timestamp, lastTimestamp,LAST;
+    long timestamp, lastTimestamp, LAST;
     private ExpansionHubMotor motorss, motorsf, motords, motordf;
-    double targetAngle = 0, targetDist, arctg, calcPower;
+    double targetAngle = 0, targetDist, calculatedAngle, calcPower;
     double kp = 1, kp2 = 0.35;
+    int direction;
     private Thread Loc = new Thread(new Runnable() {
         long encSpVal, encDrVal, encStVal;
+
         @Override
         public void run() {
-            while(!isStopRequested()){
+            while (!isStopRequested()) {
                 bulkData = expansionHub.getBulkInputData();
                 encSpVal = bulkData.getMotorCurrentPosition(encoderSpate);
                 encDrVal = bulkData.getMotorCurrentPosition(encoderDreapta);
@@ -54,66 +56,66 @@ public class AutonomEsuat extends LinearOpMode {
                 EncDr = -encDrVal;
 
                 encRot = ((EncDr - EncSt) / 2.0);
-                angle = Math.toRadians(encRot / tickPerDeg) % (2 * PI);
+                robotHeading = Math.toRadians(encRot / tickPerDeg) % (2 * PI); //TODO: De vazut daca functioneaza bine asta (ar trebui sa fie de la 0 la 2PI, si sa creasca gradual rotindu-se in sensul acelor de ceas 
 
                 lastCorrectedY = correctedY;
                 lastCorrectedX = correctedX;
 
-                correctedY = (EncSt + EncDr)/2.0;
+                correctedY = (EncSt + EncDr) / 2.0;
                 correctedX = EncSp + 0.656 * encRot;
 
                 deltaY = correctedY - lastCorrectedY;
                 deltaX = correctedX - lastCorrectedX;
 
-                cosdeltay = (Math.cos(angle) * deltaY);
-                cosdeltax = (Math.cos((PI/2)- angle) * deltaX);
-                sindeltay = -(Math.sin(angle) * deltaY);
-                sindeltax = (Math.sin((PI/2)-angle) * deltaX);
+                cosdeltay = (Math.cos(robotHeading) * deltaY);
+                cosdeltax = (Math.cos((PI / 2) - robotHeading) * deltaX);
+                sindeltay = -(Math.sin(robotHeading) * deltaY);
+                sindeltax = (Math.sin((PI / 2) - robotHeading) * deltaX);
 
-                currentY = currentY + (cosdeltay + cosdeltax) * mmPerTick;
+                currentY = currentY + (cosdeltay + cosdeltax) * mmPerTick;  //TODO: De verificat daca merg OK
                 currentX = currentX + (sindeltay + sindeltax) * mmPerTick;
             }
         }
     });
 
-    public void calcule(double X, double Y){
-
+    public void calcule(double X, double Y) {
+        boolean jumpDetected = false;
         double dx = X - currentX;
         double dy = Y - currentY;
 
-        if(dx > 0){
-            if(dy > 0){
+        if (dx > 0) {
+            if (dy > 0) {
                 //1
-                arctg = (Math.atan(dx / dy));
-            }
-            else{
+                calculatedAngle = 2*PI - (Math.atan(dx / dy));
+            } else {
                 //2
-                arctg = (Math.atan(dx / dy) + PI);
+                calculatedAngle = 2*PI - (Math.atan(dx / dy) + PI);
             }
-        }
-        else{
-            if(dy > 0){
-                //3
-                arctg = (Math.atan(dx / dy) + 2*PI);
-            }
-            else{
-
+        } else {
+            if (dy > 0) {
                 //4
-                arctg = (Math.atan(dx / dy) + PI);
+                calculatedAngle = 2*PI - (Math.atan(dx / dy) + 2 * PI);
+            } else {
+                //3
+                calculatedAngle = 2*PI - (Math.atan(dx / dy) + PI);
             }
         }
-        targetAngle = arctg + angle;
-        if(targetAngle < PI){
-            targetAngle = -targetAngle;
+        targetAngle = calculatedAngle - robotHeading;
+        if(targetAngle > PI){
+            targetAngle = 2 * PI - targetAngle;
+            direction = -1;
         }
         else{
-            targetAngle = PI - (targetAngle - PI);
+            direction = 1;
         }
+        
         //targetDist = Math.sqrt((X - currentX) * (X - currentX) + (Y - currentY) * (Y - currentY));
-        /*
-        if(targetAngle > 0){
-            while(targetAngle > 0){
-                targetAngle = angle + arctg;
+        if(direction == -1){
+            while(!jumpDetected || calculatedAngle - robotHeading <0){
+                if(lastRobotHeading < robotHeading){
+                    jumpDetected = true;
+                }
+                targetAngle = calculatedAngle - robotHeading;
                 calcPower= -(targetAngle * kp); // cu +
                 if(calcPower > 1){
                     calcPower = 1;
@@ -125,7 +127,7 @@ public class AutonomEsuat extends LinearOpMode {
         }
         else{
             while(targetAngle < 0){
-                targetAngle = angle + arctg;
+                targetAngle = robotHeading + calculatedAngle;
                 calcPower= -(targetAngle * kp); // cu +
                 if(calcPower > 1){
                     calcPower = 1;
@@ -136,24 +138,27 @@ public class AutonomEsuat extends LinearOpMode {
             }
         }
         power(0,0,0,0);
-         */
     }
-    public void miscare_fata(double targetY){
-        double tY = targetY;
-        double powerY;
-        while(currentY <= tY){
-            powerY = kp2*(targetY-currentY);
-            if(powerY>1)powerY = 1;
-            if(powerY < -1)powerY = -1;
-            power(powerY,powerY,powerY,powerY);
-        }
-        power(0,0,0,0);
 
+
+    private void miscare_fata(double targetY) {
+        double powerY;
+        while (currentY <= targetY) {
+            powerY = kp2 * (targetY - currentY);
+            if (powerY > 1) {
+                powerY = 1;
+            }
+            if (powerY < -1) {
+                powerY = -1;
+            }
+            power(powerY, powerY, powerY, powerY);
+        }
+        power(0, 0, 0, 0);
     }
 
 
     @Override
-    public void runOpMode(){
+    public void runOpMode() {
 
         expansionHub = hardwareMap.get(ExpansionHubEx.class, configs.expansionHubOdometrieName);
         motorss = (ExpansionHubMotor) hardwareMap.get(DcMotorEx.class, configs.ssName);
@@ -203,7 +208,7 @@ public class AutonomEsuat extends LinearOpMode {
 
         //power(-0.3,-0.3,0.3,0.3); // se roteste cu minus
 
-        while(!isStarted()) {
+        while (!isStarted()) {
             if (stoneDetectorModified.foundRectangles().get(0).y > 406) {
                 telemetry.addData("Skystone Position:", "LEFT");
             } else if (stoneDetectorModified.foundRectangles().get(0).y > 253) {
@@ -216,17 +221,18 @@ public class AutonomEsuat extends LinearOpMode {
             telemetry.addData("EncSp", EncSp);
             telemetry.addData("X", currentX);
             telemetry.addData("Y", currentY);
-            telemetry.addData("atan", arctg);
-            telemetry.addData("Angle ", angle);
+            telemetry.addData("atan", calculatedAngle);
+            telemetry.addData("Angle ", robotHeading);
             telemetry.addData("targetAngle", targetAngle);
             telemetry.addData("targetDistance", targetDist);
             telemetry.update();
         }
         waitForStart();
-      //  calcule(10000,10000);
-        miscare_fata(100);
+        //  calcule(10000,10000);
+        miscare_fata(1000);
     }
-    private void power(double ds, double df, double ss, double sf){
+
+    private void power(double ds, double df, double ss, double sf) {
         motordf.setPower(df);
         motorss.setPower(ss);
         motorsf.setPower(sf);
