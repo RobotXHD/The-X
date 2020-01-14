ca0e0fc7e6896fbd01e7a86d82f334ac8a5fe6bd
Rev Laptop <robotxbp@gmail.com>
2020-01-14 15:22:17 +0200

    Automatizari V1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Automatizari_config.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Automatizari_config.java
@@ -5,4 +5,5 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class Automatizari_config {
     public static double kp = 0.003, ki = 0, kd = 0.0025, setpoint = 0;
+    public static double toleranceScissorSt,toleranceScissorDr,targetVerifications;
 }
